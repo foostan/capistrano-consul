63525c3751eba1f2b2bc16f43203c455d70e0742
foostan <foostan27@gmail.com>
2014-06-21 10:37:14 +0900

    Deploy target getting by consul with attender

--- config/deploy/consul.rb
+++ config/deploy/consul.rb
@@ -1,2 +1,16 @@
-server '192.168.33.11', user: 'vagrant', password: 'vagrant', roles: %{web app}
-server '192.168.33.12', user: 'vagrant', password: 'vagrant', roles: %{web app}
+require 'attender'
+
+consul_api_endpoint = '192.168.33.101'
+port = '8500'
+attender = Attender::Agent.new(consul_api_endpoint, port)
+
+nodes = attender.get('/v1/catalog/nodes', false)
+nodes.each do |node|
+  node_name = node['Node']
+  node_ipaddr = node['Address']
+  node_info = attender.get("/v1/catalog/node/#{node_name}", false)
+  node_services = node_info['Services'].keys
+
+  server node_ipaddr, user: 'vagrant', password: 'vagrant', roles: node_services
+end
+
